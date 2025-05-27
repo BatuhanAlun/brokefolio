f40d3008728089d4611c1b137126da9d5e8bfbeb
Batuhan Alun <22220030071@mersin.edu.tr>
2025-05-27 16:39:25 +0300

    Register Handler and Database DI added.

--- backend/cmd/brokefolio/main.go
+++ backend/cmd/brokefolio/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"brokefolio/backend/internal/database"
+	"brokefolio/backend/internal/handlers"
+	"log"
 	"net/http"
+	"os"
 
-	"brokefolio.com/internal/handlers"
+	"github.com/rs/cors"
 )
 
 func main() {
 
+	db := database.InitDB()
+	defer db.Close()
+
 	router := http.NewServeMux()
 
+	registerHandler := handlers.NewRegisterHandler(db)
+
 	router.HandleFunc("POST /api/login", handlers.LoginHandler)
+	router.HandleFunc("/api/register", registerHandler.RegisterHandler)
+
+	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
+	if allowedOrigins == "" {
+		allowedOrigins = "http://127.0.0.1:5500"
+	}
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{allowedOrigins},
+		AllowedMethods: []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+		Debug:          os.Getenv("CORS_DEBUG") == "true",
+	})
+
+	handler := c.Handler(router)
+
+	log.Println("Server listening on :8080")
+	err := http.ListenAndServe(":8080", handler)
+	if err != nil {
+		log.Fatalf("ListenAndServe error: %v", err)
+	}
 }
